1406d669d5aada1952ca7951f439148cb96a1a07
peulogo <99072707+peulogo@users.noreply.github.com>
2023-11-14 23:01:58 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function(){document.body.classList.remove('loading')})
 
 const cart = document.querySelector('.card__container');
-const product = document.querySelector('.product__item');
+const products = document.querySelectorAll('.product__item');
 const popup = document.querySelector('.popup');
 const closeBtn = document.querySelector('.popup__close-btn');
 
@@ -11,10 +11,20 @@ cart.addEventListener('click', () => {
     }
 });
 
-product.addEventListener('click', () =>{
-    popup.classList.add('_active-popup');
-    document.body.classList.add('unscroll')
+products.forEach((product) => {
+    product.addEventListener('click', () =>{
+        popup.classList.add('_active-popup');
+        document.body.classList.add('unscroll')
+    })
 })
+
+products.forEach((product) => {
+    product.addEventListener('click', () =>{
+        popup.classList.add('_active-popup');
+        document.body.classList.add('unscroll')
+    })
+})
+
 closeBtn.addEventListener('click', () => {
     popup.classList.remove('_active-popup');
     document.body.classList.remove('unscroll')
